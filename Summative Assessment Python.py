d98a80e63d6df75b50a176a98f6ff1f3adb8fa7c
CutePally <61581434+CutePally@users.noreply.github.com>
2020-03-02 18:14:13 +0200

    Add files via upload

---
+++ Summative Assessment Python.py
@@ -0,0 +1,347 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  1 22:21:19 2020
+
+@author: 212687736
+"""
+
+#Palesa Molise
+
+
+
+
+
+#Problem 1 Solution
+import random
+
+print("cluster1")
+cluster1 = []
+for i in range(16):
+
+ cluster1Number = random.random()
+ cluster1.append(cluster1Number)
+
+print(cluster1)
+
+
+print("cluster2")
+cluster2 = []
+for i in range(16):
+
+ cluster2Number = random.random()
+ cluster2.append(cluster2Number)
+
+print(cluster2)
+
+
+print("cluster3")
+cluster3 = []
+for i in range(16):
+
+ cluster3Number = random.random()
+ cluster3.append(cluster3Number)
+
+print(cluster3)
+
+
+print("cluster4")
+cluster4 = []
+for i in range(16):
+
+ cluster4Number = random.random()
+ cluster4.append(cluster4Number)
+
+print(cluster4)
+
+
+print("cluster5")
+cluster5 = []
+for i in range(16):
+
+ cluster5Number = random.random()
+ cluster5.append(cluster5Number)
+
+print(cluster5)
+
+
+print("cluster6")
+cluster6 = []
+for i in range(16):
+
+ cluster6Number = random.random()
+ cluster6.append(cluster6Number)
+
+print(cluster6)
+
+
+print("cluster7")
+cluster7 = []
+for i in range(16):
+
+ cluster7Number = random.random()
+ cluster7.append(cluster7Number)
+
+print(cluster7)
+
+
+print("cluster8")
+cluster8 = []
+for i in range(16):
+
+ cluster8Number = random.random()
+ cluster8.append(cluster8Number)
+
+print(cluster8)
+
+print("cluster9")
+cluster9 = []
+for i in range(16):
+
+ cluster9Number = random.random()
+ cluster9.append(cluster9Number)
+
+print(cluster9)
+
+
+print("cluster10")
+cluster10 = []
+for i in range(16):
+
+ cluster10Number = random.random()
+ cluster10.append(cluster10Number)
+
+print(cluster10)
+
+
+print("cluster11")
+cluster11 = []
+for i in range(16):
+
+ cluster11Number = random.random()
+ cluster11.append(cluster11Number)
+
+print(cluster11)
+
+
+print("cluster12")
+cluster12 = []
+for i in range(16):
+
+ cluster12Number = random.random()
+ cluster12.append(cluster12Number)
+
+print(cluster12)
+
+
+print("cluster13")
+cluster13 = []
+for i in range(16):
+
+ cluster13Number = random.random()
+ cluster13.append(cluster13Number)
+
+print(cluster13)
+
+
+print("cluster14")
+cluster14 = []
+for i in range(16):
+
+ cluster14Number = random.random()
+ cluster14.append(cluster14Number)
+
+print(cluster14)
+
+
+print("cluster15")
+cluster15 = []
+for i in range(16):
+
+ cluster15Number = random.random()
+ cluster15.append(cluster15Number)
+
+print(cluster15)
+
+
+print("cluster16")
+cluster16 = []
+for i in range(16):
+
+ cluster16Number = random.random()
+ cluster16.append(cluster16Number)
+
+print(cluster16)
+
+
+print("cluster17")
+cluster17 = []
+for i in range(16):
+
+ cluster17Number = random.random()
+ cluster17.append(cluster17Number)
+
+print(cluster17)
+
+
+
+print("cluster18")
+cluster18 = []
+for i in range(16):
+
+ cluster18Number = random.random()
+ cluster18.append(cluster18Number)
+
+print(cluster18)
+
+
+print("cluster19")
+cluster19 = []
+for i in range(16):
+
+ cluster19Number = random.random()
+ cluster19.append(cluster19Number)
+
+print(cluster19)
+
+
+print("cluster20")
+cluster20 = []
+for i in range(16):
+
+ cluster20Number = random.random()
+ cluster20.append(cluster20Number)
+
+print(cluster20)
+
+
+print("cluster21")
+cluster21 = []
+for i in range(16):
+
+ cluster21Number = random.random()
+ cluster21.append(cluster21Number)
+
+print(cluster21)
+
+
+
+print("cluster22")
+cluster22 = []
+for i in range(16):
+
+ cluster22Number = random.random()
+ cluster22.append(cluster22Number)
+
+print(cluster22)
+
+
+print("cluster23")
+cluster23 = []
+for i in range(16):
+
+ cluster23Number = random.random()
+ cluster23.append(cluster23Number)
+
+print(cluster23)
+
+
+print("cluster24")
+cluster24 = []
+for i in range(16):
+
+ cluster24Number = random.random()
+ cluster24.append(cluster24Number)
+
+print(cluster24)
+
+
+print("cluster25")
+cluster25 = []
+for i in range(16):
+
+ cluster25Number = random.random()
+ cluster25.append(cluster25Number)
+
+print(cluster25)
+
+
+print("cluster26")
+cluster26 = []
+for i in range(16):
+
+ cluster26Number = random.random()
+ cluster26.append(cluster26Number)
+
+print(cluster26)
+
+
+print("cluster27")
+cluster27 = []
+for i in range(16):
+
+ cluster27Number = random.random()
+ cluster27.append(cluster27Number)
+
+print(cluster27)
+
+
+print("cluster28")
+cluster28 = []
+for i in range(16):
+
+ cluster28Number = random.random()
+ cluster28.append(cluster28Number)
+
+print(cluster28)
+      
+      
+print("cluster29")
+cluster29 = []
+for i in range(16):
+
+ cluster29Number = random.random()
+ cluster29.append(cluster29Number)
+
+print(cluster29)
+      
+      
+print("cluster30")
+cluster30 = []
+for i in range(16):
+
+ cluster30Number = random.random()
+ cluster30.append(cluster30Number)
+
+print(cluster30)
+
+
+print("cluster31")
+cluster31 = []
+for i in range(16):
+
+ cluster31Number = random.random()
+ cluster31.append(cluster31Number)
+
+print(cluster31)
+
+
+print("cluster32")
+cluster32 = []
+for i in range(16):
+
+ cluster32Number = random.random()
+ cluster32.append(cluster32Number)
+
+print(cluster32)
+
+
+#Problem 2 Solution
+
+import time
+print(input('Enter data reference to generate new data file: '))
+DateStamp=time.strftime("%Y-%b-%d__%H_%M_%S",time.localtime())
+sensors = open('pipiline'+Datestamp+'.log', 'w')
+
+sensors.close()
+    
+
